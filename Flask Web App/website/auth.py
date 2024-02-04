deb6327734e69d6a62bd6fcaf02e366dd13902e0
Kuba <kkuriata32@gmail.com>
2024-02-03 22:55:44 -0800

    beginning of debugging

--- Flask Web App/website/auth.py
+++ Flask Web App/website/auth.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for # Import the Blueprint class from the flask package
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 # Create a Blueprint object --> meaning it has a bunch of routes/URLs
 auth = Blueprint('auth', __name__) # The first argument is the name of the blueprint, and the second argument is the name of the module or package
 
 @auth.route('/login', methods=['GET', 'POST']) # This is a decorator that tells Flask what URL to trigger the function
 def login():
-    return render_template("login.html", text="Testing", user="Jake", boolean = True) # This is the function that will be triggered when the URL is visited
+    return render_template("login.html", boolean = True) # This is the function that will be triggered when the URL is visited
 
 @auth.route('/logout') # This is a decorator that tells Flask what URL to trigger the function
 def logout():
@@ -17,13 +18,13 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be longer than 3 characters!', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First Name must be at least 2 characters long!', category='error')
         elif password1 != password2:
             flash('Passwords do not match!', category='error')
@@ -31,7 +32,7 @@ def sign_up():
             flash('Password must be at least 8 characters.', category='error')
         else:
             # add user to database
-            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method ='sha256'))
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method ='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='success')
--- Flask Web App/website/models.py
+++ Flask Web App/website/models.py
@@ -1,6 +1,6 @@
-from .import db
+from . import db
 from flask_login import UserMixin
-from sqalchemy.sql import func
+from sqlalchemy.sql import func
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)

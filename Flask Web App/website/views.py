dae56c9c9306a7e7e5989178c2137b4dd9d17aea
JakeMerrill <58450076+JakeMerrill@users.noreply.github.com>
2024-02-29 13:27:58 -0800

    Created the functionality to pull all the workouts from the data base for the workout page

--- Flask Web App/website/views.py
+++ Flask Web App/website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for # Import the Blueprint class from the flask package
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, UserWorkout
 from . import db
 import json
 from .access import DbAccessSingleton
@@ -14,22 +14,55 @@ views = Blueprint('views', __name__) # The first argument is the name of the blu
 @login_required # This is a decorator that tells Flask we need to login before accessing the home page
 def home():
     if request.method == 'POST':
-        # note = request.form.get('note')
-        
-        # if len(note) < 1:
-        #     flash('Note is too short!', category='error')
-        # else:
-        #     new_note = Note(data=note, user_id=current_user.id)
-        #     db.session.add(new_note)
-        #     db.session.commit()
-        #     flash('Note added!', category='success')
         muscle_group = request.form.get('MuscleGroup')
         equipment = request.form.get('Equipment')
 
         return redirect(url_for('views.result', muscle_group=muscle_group, equipment=equipment))
 
     else:
-        return render_template("Home.html", user=current_user) # This is the function that will be triggered when the URL is visited
+        workoutNames = []
+        workoutList = []
+        db_instance = DbAccessSingleton.get_instance()
+        result = db_instance.custom_query("SELECT DISTINCT WorkoutName FROM SavedWorkouts WHERE UserID = " + f"'{current_user.email}'")
+        result = list(result)
+        for name in result:
+            tempName = name[0]
+            workoutNames.append(tempName)
+        for name in workoutNames:
+            workoutObj = UserWorkout({'name': name, 'exercises': []})
+            workoutObj.getWorkoutDB()
+            workoutList.append(workoutObj)
+        for workout in workoutList:
+            workout.printWorkout()
+            print("\n")
+            
+        # workoutObj = UserWorkout({'name': workoutName, 'exercises': []})
+        # workoutObj.getWorkoutDB(workoutName)
+        # workoutObj.printWorkout()
+        return render_template("Home.html", user=current_user)
+    
+
+
+# @views.route('/', methods=['GET', 'POST']) # This is a decorator that tells Flask what URL to trigger the function
+# @login_required # This is a decorator that tells Flask we need to login before accessing the home page
+# def home():
+#     if request.method == 'POST':
+#         # note = request.form.get('note')
+        
+#         # if len(note) < 1:
+#         #     flash('Note is too short!', category='error')
+#         # else:
+#         #     new_note = Note(data=note, user_id=current_user.id)
+#         #     db.session.add(new_note)
+#         #     db.session.commit()
+#         #     flash('Note added!', category='success')
+#         muscle_group = request.form.get('MuscleGroup')
+#         equipment = request.form.get('Equipment')
+
+#         return redirect(url_for('views.result', muscle_group=muscle_group, equipment=equipment))
+
+#     else:
+#         return render_template("Home.html", user=current_user) # This is the function that will be triggered when the URL is visited
 
 @views.route('/Result')
 def result():

5f920fb9ed5121ae1e2dbaec58c874125986cb9f
JakeMerrill <58450076+JakeMerrill@users.noreply.github.com>
2024-03-04 14:17:27 -0800

    Added WorkoutViewer html and js files

---
+++ Flask Web App/website/static/WorkoutViewer.js
@@ -0,0 +1,29 @@
+// JavaScript to handle starting workout
+document.addEventListener('DOMContentLoaded', function() {
+    const startButtons = document.querySelectorAll('.start-workout-btn');
+    startButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const workoutName = this.dataset.workout;
+            const workoutData = JSON.parse(this.dataset.workoutData);
+
+            $.ajax({
+                type: 'POST',
+                url: '/save_workout_data',  // Use the new route for saving workout data
+                contentType: 'application/json',
+                data: JSON.stringify({ workoutName: workoutName, workoutData: workoutData }),
+                success: function(response) {
+                    // Handle success response from the server if needed
+                    console.log('Workout data sent successfully.');
+                    console.log('Server response:', response);
+                    // Perform any additional actions as needed
+                    window.location.href = '/Result';
+                },
+                error: function(xhr, status, error) {
+                    // Handle error response from the server if needed
+                    window.location.href = '/WorkoutPage';
+                    console.error('Error sending workout data:', error);
+                }
+            });
+        });
+    });
+});
